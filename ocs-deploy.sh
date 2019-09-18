568837e1db0137223bf9dd986e97ac24beb2b1c1
Naveen Malik <nmalik@redhat.com>
2019-09-18 15:30:33 -0400

    Tweaks to deploy script

--- ocs-deploy.sh
+++ ocs-deploy.sh
@@ -8,13 +8,27 @@ cd "$SCRIPT_DIR" || exit 1
 # And machinesets for OCS
 OCS_INSTANCE_TYPE="m4.xlarge"
 OCS_PER_AZ=1
-MACHINESETS=$("$OC" -n openshift-machine-api get machinesets -o custom-columns=name:metadata.name,replicas:spec.replicas --no-headers  | grep ' 1' | grep -v ocs | awk '{ print $1 }')
+MACHINESETS=$("$OC" -n openshift-machine-api get machinesets -o custom-columns=name:metadata.name,replicas:spec.replicas --no-headers  | grep 'worker' | grep -v ocs | awk '{ print $1 }')
 for ms in $MACHINESETS; do
         "$OC" -n openshift-machine-api get "machinesets/$ms" -ojson |\
         jq --arg inst "$OCS_INSTANCE_TYPE" --arg rep $OCS_PER_AZ '.metadata.name=.metadata.name+"-ocs"|.spec.selector.matchLabels."machine.openshift.io/cluster-api-machineset"=.metadata.name|.spec.template.metadata.labels."machine.openshift.io/cluster-api-machineset"=.metadata.name|.spec.template.metadata.labels."cluster.ocs.openshift.io/openshift-storage" = ""|.spec.template.spec.providerSpec.value.instanceType=$inst|.spec.replicas=($rep|tonumber)|.spec.template.spec.metadata.labels."cluster.ocs.openshift.io/openshift-storage" = ""' |\
         "$OC" -n openshift-machine-api apply -f -
 done
 "$OC" -n openshift-machine-api get machinesets
+
+echo -n "Waiting for all replicas..."
+while true;
+do
+    COUNT=$(oc get machinesets -A -o json  | jq -r '.items[] | select(.status.replicas != .status.availableReplicas) | .metadata.name' | wc -l)
+    if [ "$COUNT" != "0" ];
+    then
+        echo -n "."
+        sleep 15
+    else
+        echo "done"
+        break
+    fi
+done
 
 NAMESPACE="openshift-storage"
 OCS_PATH="https://raw.githubusercontent.com/openshift/ocs-operator/master"
